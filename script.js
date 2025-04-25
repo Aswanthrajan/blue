2164ec272fa131b97c72bd05d7df3518d271b7c1
ASWANTHRAJAN.A.N <112618992+Aswanthrajan@users.noreply.github.com>
2025-04-25 14:32:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function togglemenu(){
-    const menu=document.querySelector(".menu-links");
-    const icon=document.querySelector(".hamburger-icon");
-    document.querySelector(".menu-links-wrapper").classList.toggle("active"); 
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
